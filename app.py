65e72108aabe8ed54b4aeb3d598b07539dc64381
Kyle <c0010105@newcastle.ac.uk>
2021-12-05 15:12:29 +0000

    moves websiteResources to static folder, implemented templates for each page and created working links for each of them.

--- app.py
+++ app.py
@@ -1,11 +1,46 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():  # put application's code here
+    return render_template('index.html')
+
+
+@app.route('/account')
+def account():  # put application's code here
+    return render_template('account.html')
+
+
+@app.route('/admin')
+def admin():  # put application's code here
+    return render_template('admin.html')
+
+
+@app.route('/learningResources')
+def learningResources():  # put application's code here
+    return render_template('learningResources.html')
+
+
+@app.route('/leaderboard')
+def leaderboard():  # put application's code here
+    return render_template('leaderboard.html')
+
+
+@app.route('/aboutUs')
+def aboutUs():  # put application's code here
+    return render_template('aboutUs.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 
 if __name__ == '__main__':
