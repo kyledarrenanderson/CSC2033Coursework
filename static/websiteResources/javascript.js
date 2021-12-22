4aeeaf50d8dcdcffd5a85900f1f1f00e9409f3a0
SonicElvis <elvis.e.robinson@gmail.com>
2021-12-22 18:04:20 +0000

    Added base quiz system

---
+++ static/websiteResources/javascript.js
@@ -0,0 +1,25 @@
+data =
+    '[{"Difficulty" : "Easy", "question" : "An easy question"},' +
+    '{"Difficulty" : "Easy", "question" : "Another easy question"},' +
+    '{"Difficulty" : "Easy", "question" : "Another easier question"},' +
+    '{"Difficulty" : "Easy", "question" : "A question a bit less easy"},' +
+    '{"Difficulty" : "Easy", "question" : "A question a lot more easy"},' +
+    '{"Difficulty" : "Easy", "question" : "A question that is rather easy"},' +
+    '{"Difficulty" : "Easy", "question" : "A question with an easy answer"},' +
+    '{"Difficulty" : "Easy", "question" : "A easy enough question"},' +
+    '{"Difficulty" : "Normal", "question" : "1"},' +
+    '{"Difficulty" : "Normal", "question" : "2"},' +
+    '{"Difficulty" : "Normal", "question" : "3"},' +
+    '{"Difficulty" : "Normal", "question" : "4"},' +
+    '{"Difficulty" : "Normal", "question" : "5"},' +
+    '{"Difficulty" : "Normal", "question" : "6"},' +
+    '{"Difficulty" : "Normal", "question" : "7"},' +
+    '{"Difficulty" : "Normal", "question" : "8"},' +
+    '{"Difficulty" : "Hard", "question" : "1"},' +
+    '{"Difficulty" : "Hard", "question" : "2"},' +
+    '{"Difficulty" : "Hard", "question" : "3"},' +
+    '{"Difficulty" : "Hard", "question" : "4"},' +
+    '{"Difficulty" : "Hard", "question" : "5"},' +
+    '{"Difficulty" : "Hard", "question" : "6"},' +
+    '{"Difficulty" : "Hard", "question" : "7"},' +
+    '{"Difficulty" : "Hard", "question" : "8"}]';
