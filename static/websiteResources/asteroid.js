1cfb227d17cbbf1b2e82acb7d49206431f4f7842
c0021401 <c0021401@newcastle.ac.uk>
2021-12-30 19:46:29 +0000

    Player shoots bullet at mouse on click

--- static/websiteResources/asteroid.js
+++ static/websiteResources/asteroid.js
@@ -3,7 +3,7 @@ window.addEventListener("DOMContentLoaded", game);
 
 // Load Game Sprites
 var sprite = new Image();
-sprite.src = "https://i.imgur.com/ieMCMXc.png";
+sprite.src = "https://i.imgur.com/WhFvtSa.png";
 
 function game() {
 
@@ -13,34 +13,70 @@ function game() {
         canvasWidth = context.canvas.width = window.innerWidth,
         canvasHeight = context.canvas.height = window.innerHeight;
 
-    var _player = {rotation: 0};
+    var bullets = [];
+    var asteroids = [];
+    var score = 0;
+    var gameActive = true;
 
-    canvas.addEventListener('click', shoot);
-    canvas.addEventListener('mousemove', aim);
+    var _player = {
+        rotation: 0,
+        rotationTrue: 0};
 
-    function shoot(obj) {
+    canvas.addEventListener('click', aimShoot);
 
-    }
-
-    // rotate the player to mouse position
-    function aim(obj) {
-        _player.rotation = Math.atan2(obj.offsetX - canvasWidth/2, -(obj.offsetY - canvasHeight/2));
-    }
-
+    // player
     function player() {
         context.save();
-
-
+        // interpolation needs to rotate via shortest angle
+        _player.rotationTrue = interlope(_player.rotationTrue,
+                                        _player.rotation,
+                                        1)
         context.translate(canvasWidth/2, canvasHeight/2);
-        context.rotate(_player.rotation );
+        context.rotate(_player.rotationTrue);
         context.drawImage(sprite, 0, 0, 64, 128, -32, -64, 64, 128);
         context.restore();
+        gameUpdate();
+    }
+
+    //create new bullet
+    function createBullet(id, bulletAngle) {
+        var bullet = {
+            x : canvasWidth/2,
+            y : canvasHeight/2,
+            angle : bulletAngle,
+            hit : false,
+            answerID : id
+        }
+        bullets.push(bullet);
+    }
+
+    // rotate the player to mouse position and shoot
+    function aimShoot(obj) {
+        _player.rotation = Math.atan2(obj.offsetX - canvasWidth/2, -(obj.offsetY - canvasHeight/2));
+        createBullet(0, _player.rotation);
+    }
+
+    // manages global game updating
+    function gameUpdate() {
+        for (var i = 0;  i < bullets.length; i++) {
+            if (!bullets[i].hit) {
+                context.save();
+                context.translate(canvasWidth/2,canvasHeight/2);
+                context.rotate(bullets[i].angle);
+                context.drawImage(sprite, 65, 0, 96, 32, bullets[i].x-canvasWidth/2-16-8, bullets[i].y-canvasHeight/2-16, 96, 32);
+                context.restore();
+
+                bullets[i].x = bullets[i].x + 15 * Math.cos(bullets[i].angle/180- Math.PI/2);
+                bullets[i].y = bullets[i].y + 15 * Math.sin(bullets[i].angle/180- Math.PI/2);
+            }
+        }
     }
 
     function startGame() {
         context.clearRect(0, 0, canvasWidth, canvasHeight);
         context.beginPath();
         player();
+
     }
 
     function init() {
@@ -49,4 +85,9 @@ function game() {
     }
 
     init();
+
+    // utility functions
+    function interlope(a, b, x) {
+      return a * (1-x) + b * x;
+    }
 }
