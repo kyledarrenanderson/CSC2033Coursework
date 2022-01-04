c5e5912af1d92ae364e287d983230fc9408e7376
c0021401 <c0021401@newcastle.ac.uk>
2022-01-04 13:53:03 +0000

    Update data to have answers

--- static/websiteResources/javascript.js
+++ static/websiteResources/javascript.js
@@ -1,25 +1,25 @@
 data =
-    '[{"Difficulty" : "Easy", "question" : "An easy question"},' +
-    '{"Difficulty" : "Easy", "question" : "Another easy question"},' +
-    '{"Difficulty" : "Easy", "question" : "Another easier question"},' +
-    '{"Difficulty" : "Easy", "question" : "A question a bit less easy"},' +
-    '{"Difficulty" : "Easy", "question" : "A question a lot more easy"},' +
-    '{"Difficulty" : "Easy", "question" : "A question that is rather easy"},' +
-    '{"Difficulty" : "Easy", "question" : "A question with an easy answer"},' +
-    '{"Difficulty" : "Easy", "question" : "A easy enough question"},' +
-    '{"Difficulty" : "Normal", "question" : "1"},' +
-    '{"Difficulty" : "Normal", "question" : "2"},' +
-    '{"Difficulty" : "Normal", "question" : "3"},' +
-    '{"Difficulty" : "Normal", "question" : "4"},' +
-    '{"Difficulty" : "Normal", "question" : "5"},' +
-    '{"Difficulty" : "Normal", "question" : "6"},' +
-    '{"Difficulty" : "Normal", "question" : "7"},' +
-    '{"Difficulty" : "Normal", "question" : "8"},' +
-    '{"Difficulty" : "Hard", "question" : "1"},' +
-    '{"Difficulty" : "Hard", "question" : "2"},' +
-    '{"Difficulty" : "Hard", "question" : "3"},' +
-    '{"Difficulty" : "Hard", "question" : "4"},' +
-    '{"Difficulty" : "Hard", "question" : "5"},' +
-    '{"Difficulty" : "Hard", "question" : "6"},' +
-    '{"Difficulty" : "Hard", "question" : "7"},' +
-    '{"Difficulty" : "Hard", "question" : "8"}]';
+    '[{"Difficulty" : "Easy", "question" : "An easy question", "answer" : "An easy question"},' +
+    '{"Difficulty" : "Easy", "question" : "Another easy question", "answer" : "Another easy question"},' +
+    '{"Difficulty" : "Easy", "question" : "Another easier question", "answer" : "Another easier question"},' +
+    '{"Difficulty" : "Easy", "question" : "A question a bit less easy", "answer" : "A question a bit less easy"},' +
+    '{"Difficulty" : "Easy", "question" : "A question a lot more easy", "answer" : "A question a lot more easy"},' +
+    '{"Difficulty" : "Easy", "question" : "A question that is rather easy", "answer" : "A question that is rather easy"},' +
+    '{"Difficulty" : "Easy", "question" : "A question with an easy answer", "answer" : "A question with an easy answer"},' +
+    '{"Difficulty" : "Easy", "question" : "A easy enough question", "answer" "A easy enough question": },' +
+    '{"Difficulty" : "Normal", "question" : "1", "answer" : "1"},' +
+    '{"Difficulty" : "Normal", "question" : "2", "answer" : "2"},' +
+    '{"Difficulty" : "Normal", "question" : "3", "answer" : "3"},' +
+    '{"Difficulty" : "Normal", "question" : "4", "answer" : "4"},' +
+    '{"Difficulty" : "Normal", "question" : "5", "answer" : "5"},' +
+    '{"Difficulty" : "Normal", "question" : "6", "answer" : "6"},' +
+    '{"Difficulty" : "Normal", "question" : "7", "answer" : "7"},' +
+    '{"Difficulty" : "Normal", "question" : "8", "answer" : "8"},' +
+    '{"Difficulty" : "Hard", "question" : "1", "answer" : "1"},' +
+    '{"Difficulty" : "Hard", "question" : "2", "answer" : "2"},' +
+    '{"Difficulty" : "Hard", "question" : "3", "answer" : "3"},' +
+    '{"Difficulty" : "Hard", "question" : "4", "answer" : "4"},' +
+    '{"Difficulty" : "Hard", "question" : "5", "answer" : "5"},' +
+    '{"Difficulty" : "Hard", "question" : "6", "answer" : "6"},' +
+    '{"Difficulty" : "Hard", "question" : "7", "answer" : "7"},' +
+    '{"Difficulty" : "Hard", "question" : "8", "answer" : "8"}]';
