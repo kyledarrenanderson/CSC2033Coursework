df96bfb987078a330ca476ee4f5f2a1c480c561e
Kyle <c0010105@newcastle.ac.uk>
2022-01-17 21:45:34 +0000

    Added update Account Information Page, Added Total Score Leaderboard

--- users/forms.py
+++ users/forms.py
@@ -42,3 +42,28 @@ class LoginForm(FlaskForm):
     email = StringField(validators=[data_required(), Email()])
     password = PasswordField(validators=[data_required()])
     submit = SubmitField()
+
+
+class UpdateInfoForm(FlaskForm):
+    firstName = StringField(validators=[character_check])
+    lastName = StringField(validators=[character_check])
+    email = StringField(validators=[Email()])
+    phone = StringField()
+    password = PasswordField(validators=[Length(min=8, message='password must be 8 characters or more')])
+    confirmPassword = PasswordField(validators=[EqualTo('password', message='Passwords do not match')])
+    educationLevel = SelectField(label='Education Level', choices=('None', 'GCSE', 'A Level', 'Bachelors', 'Masters'),)
+    studiedCompSci = SelectField(label='Studied Computer Science', choices=('No', 'Yes'), validators=[data_required()])
+    currentPassword = PasswordField(validators=[data_required()])
+    submit = SubmitField()
+
+    def validate_password(self, password):
+
+        p = re.compile(r'(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[@_!$%^&*()<>?/}{~:#])')
+        if not p.match(self.password.data):
+            raise ValidationError("Password must contain at least 1 digit, 1 uppercase letter,"
+                                  " 1 lower case letter and a special character.")
+
+    def validate_phone(self, phone):
+        n = re.compile(r'((\d){4}(-)(\d){3}(-)(\d){4})')
+        if not n.match(self.phone.data):
+            raise ValidationError("Phone numbers must be entered in the format XXXX-XXX-XXXX")
--- users/views.py
+++ users/views.py
@@ -8,7 +8,7 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import data_required, Email
 from werkzeug.security import check_password_hash, generate_password_hash
 from sql_handler import sql_update, sql_get
-from users.forms import RegisterForm, LoginForm
+from users.forms import RegisterForm, LoginForm, UpdateInfoForm
 from models import User
 from app import requires_roles
 
@@ -63,7 +63,6 @@ def login():
 
             return render_template('login.html', form=form)
 
-
         session['logins'] = 0
         login_user(user)
         logging.warning('SECURITY - Log in [%s, %s]', current_user.email, request.remote_addr)
@@ -87,7 +86,35 @@ def learningResources():  # put application's code here
 @login_required
 @requires_roles('user')
 def leaderboard():  # put application's code here
-    return render_template('leaderboard.html')
+
+    totalValues = sql_get("SELECT TOP 10 firstName, overallScore FROM Users WHERE role = 'user' ORDER BY overallScore DESC",())
+    return render_template('leaderboard.html', totalScores=totalValues)
+
+
+@users_blueprint.route('/accountUpdate', methods=['GET', 'POST'])
+@login_required
+@requires_roles('user')
+def accountUpdate():
+    form = UpdateInfoForm()
+    if form.validate_on_submit():
+        if form.email.data:
+            sql_update("INSERT INTO Users (email)VALUES(?) WHERE (userID)"+str(current_user.userID), form.email.data)
+        if form.firstName.data:
+            sql_update("INSERT INTO Users (firstName)VALUES(?) WHERE (userID)"+str(current_user.userID), form.firstName.data)
+        if form.lastName.data:
+            sql_update("INSERT INTO Users (lastName)VALUES(?) WHERE (userID)"+str(current_user.userID), form.lastName.data)
+        if form.phone.data:
+            sql_update("INSERT INTO Users (phoneNumber)VALUES(?) WHERE (userID)"+str(current_user.userID), form.phone.data)
+        if form.password.data:
+            sql_update("INSERT INTO Users (password)VALUES(?) WHERE (userID)"+str(current_user.userID), generate_password_hash(form.password.data))
+        if form.educationLevel.data:
+            sql_update("INSERT INTO Users (educationLevel)VALUES(?) WHERE (userID)"+str(current_user.userID), form.educationLevel.data)
+        if form.studiedCompSci.data:
+            sql_update("INSERT INTO Users (takenCS)VALUES(?) WHERE (userID)"+str(current_user.userID), form.studiedCompSci.data)
+        return redirect(url_for('users.account'))
+    return render_template("accountUpdate.html", form=form)
+
+
 
 @users_blueprint.route("/logout")
 @login_required
