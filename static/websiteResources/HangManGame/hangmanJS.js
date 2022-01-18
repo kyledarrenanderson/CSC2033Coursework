ae0b92f7b0c307442101b4522526d75c48c0fb28
c0035604 <AmongWavesPoor22042002>
2022-01-18 22:29:18 +0000

    Added one set of questions to the hangman game

--- static/websiteResources/HangManGame/hangmanJS.js
+++ static/websiteResources/HangManGame/hangmanJS.js
@@ -1,19 +1,34 @@
 //Word and Defintion list
-const randomWords = new Map([
-        ['python', 'programming language'],
-        ['javascript', 'programming language'],
-        ['mongodb', 'programming language'],
-        ['json', 'programming language'],
-        ['java', 'programming language'],
-        ['html', 'programming language'],
-        ['css', 'programming language'],
-        ['c', 'programming language'],
-        ['csharp', 'programming language'],
-        ['golang', 'programming language'],
-        ['kotlin', 'programming language'],
-        ['php', 'programming language'],
-        ['sql', 'programming language'],
-        ['ruby', 'programming language']
+let randomWords = new Map([
+        ['requirement_analysis', 'What is the first phase of Software Testing Life Cycle(STLC)?'],
+        ['manual_testing', 'Which type of software testing can be done|without programming knowledge?'],
+        ['test_tool_selection', 'What is followed on first stage in automation testing?'],
+        ['user_requirements', 'What acceptance test is based on?'],
+        ['white_box_testing', 'Which testing approach known as “structural testing”?'],
+        ['agile', 'In which SDLC methodology it is easy to change project requirements?'],
+        ['sdlc', 'V-model is a model of ---?'],
+        ['environment_setup', 'What is the 3rd step of STLC?'],
+        ['bugs', 'What are the software mistakes that occurs during in the coding phase?'],
+        ['unit_testing', 'Which testing strategy helps developers to know if the individual unit of the code is working properly?'],
+        ['test_environment_setup', 'programming language'],
+        ['waterfall_model', 'In which sequential Software Development Life Cycle(SDLC) model testing phase starts only after implementation of the system is done?'],
+        ['non_functional_testing', 'Which type of testing’s purpose is to test the reading speed of the software system?'],
+        ['requirement_analysis', 'At which stage of STLC, requirements are clarified with stakeholders?'],
+        ['grey_box_testing', 'A testing technique to test the application with partial knowledge of the internal workings.'],
+        ['integration_testing', 'A level of testing mainly used to test the data flow from one module to other module.'],
+        ['endurance_testing', 'A type of software testing method where developers test the system performance under certain load conditions over an extensive period.'],
+        ['black_box_testing', 'Which technique is applied for usability testing?'],
+        ['automation_testing', 'Which testing type offers better application with less effort?'],
+        ['static_testing', 'A type of software testing method which is used to|check the application without executing the code.'],
+        ['decision_table_testing', 'A black box testing technique that uses a systematic approach where the various input combinations and their following system behaviour are captured in a tabular form.'],
+        ['error_guessing_technique', 'A testing technique in which there is no method for identifying the error and it is based on the experience of the test analyst, where the developer uses their experience to guess the problematic areas of the software.'],
+        ['security_testing', 'A type of software testing that is intended to discover the weaknesses,|flaws of a software application as well as protect data.'],
+        ['acceptance_testing', 'In this type of software testing, users and customers determine whether the software is conforming specified requirements or not.", "answer'],
+        ['gui_testing_tool', 'This type of software tool is used to test the user interface of the application and loopholes can be identified quickly with using this tool rather than testing manually.'],
+        ['cross_browser_testing_tool', 'A type of non-functional testing tool that enables to compare|a web application in the various web browser platforms.'],
+        ['unit_testing_tool', 'A type of testing tool which helps the programmers to|improve their code. Thus, it reduces the time spent on coding.'],
+        ['performance_testing_tool', 'This type of testing tool is used to check the software application’s load, stability and scalability.'],
+        ['mutation_testing', 'A white box testing type where developers insert errors purposely into the program to verify whether the existing test case can detect the error or not.']
 ]);
 
 //making a list of only keys from words
@@ -37,7 +52,7 @@ function randomWord() {
 }
 //Generating the Keyboard
 function generateButtons() {
-    let buttonsHTML = 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter => `
+    let buttonsHTML = 'abcdefghijklmnopqrstuvwxyz_'.split('').map(letter => `
         <button
             class="btn btn-lg btn-primary m-2"
             id='` + letter + `'
@@ -71,7 +86,7 @@ function handleGuess(chosenLetter) {
 }
 //Function that updates the picture if you get a wrong guess
 function updateHangmanPicture() {
-    document.getElementById('hangmanPic').src = './hangmanimages/' + mistakes + '.jpg';
+    document.getElementById('hangmanPic').src = '../static/websiteResources/HangManGame/hangmanimages/' + mistakes + '.jpg';
 }
 //checking if the player has completely guessed the word
 function checkIfGameWon() {
@@ -115,7 +130,7 @@ function getDefintion() {
 function nextRound() {
     mistakes = 0;
     guessed = [];
-    document.getElementById('hangmanPic').src = './hangmanimages/0.jpg';
+    document.getElementById('hangmanPic').src = '../static/websiteResources/HangManGame/hangmanimages/0.jpg';
 
     randomWord();
     guessedWord();
@@ -128,7 +143,7 @@ function playAgain() {
     score = 0;
     mistakes = 0;
     guessed = [];
-    document.getElementById('hangmanPic').src = './hangmanimages/0.jpg';
+    document.getElementById('hangmanPic').src = '../static/websiteResources/HangManGame/hangmanimages/0.jpg';
 
     updateScore();
     randomWord();
@@ -168,7 +183,6 @@ function timer(seconds) {
             document.getElementById('mistakes').innerHTML = '';
             document.getElementById('wrong-guesses').innerHTML = 'Your Score is';
             document.getElementById('your-score').innerHTML = finalScore;
-            document.getElementById('hangmanPic').src="./hangmanimages/.jpg";
             return;
         }
         displayTimeLeft(secondsleft);
