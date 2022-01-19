8e877b0f431d61ab0e9aff5eaf744b1a23d626e4
Kyle <c0010105@newcastle.ac.uk>
2022-01-19 13:05:32 +0000

    Added Business intelligence questions, added alert for hangmanTO.js game regarding missing questions

--- static/websiteResources/questions_businessIntel.js
+++ static/websiteResources/questions_businessIntel.js
@@ -1,25 +1,26 @@
 data =
-    '[{"Difficulty" : "Easy", "question" : "An easy question", "answer" : "An easy question"},' +
-    '{"Difficulty" : "Easy", "question" : "Another easy question", "answer" : "Another easy question"},' +
-    '{"Difficulty" : "Easy", "question" : "Another easier question", "answer" : "Another easier question"},' +
-    '{"Difficulty" : "Easy", "question" : "A question a bit less easy", "answer" : "A question a bit less easy"},' +
-    '{"Difficulty" : "Easy", "question" : "A question a lot more easy", "answer" : "A question a lot more easy"},' +
-    '{"Difficulty" : "Easy", "question" : "A question that is rather easy", "answer" : "A question that is rather easy"},' +
-    '{"Difficulty" : "Easy", "question" : "A question with an easy answer", "answer" : "A question with an easy answer"},' +
-    '{"Difficulty" : "Easy", "question" : "A easy enough question", "answer" : "A easy enough question" },' +
-    '{"Difficulty" : "Normal", "question" : "1", "answer" : "1"},' +
-    '{"Difficulty" : "Normal", "question" : "2", "answer" : "2"},' +
-    '{"Difficulty" : "Normal", "question" : "3", "answer" : "3"},' +
-    '{"Difficulty" : "Normal", "question" : "4", "answer" : "4"},' +
-    '{"Difficulty" : "Normal", "question" : "5", "answer" : "5"},' +
-    '{"Difficulty" : "Normal", "question" : "6", "answer" : "6"},' +
-    '{"Difficulty" : "Normal", "question" : "7", "answer" : "7"},' +
-    '{"Difficulty" : "Normal", "question" : "8", "answer" : "8"},' +
-    '{"Difficulty" : "Hard", "question" : "1", "answer" : "1"},' +
-    '{"Difficulty" : "Hard", "question" : "2", "answer" : "2"},' +
-    '{"Difficulty" : "Hard", "question" : "3", "answer" : "3"},' +
-    '{"Difficulty" : "Hard", "question" : "4", "answer" : "4"},' +
-    '{"Difficulty" : "Hard", "question" : "5", "answer" : "5"},' +
-    '{"Difficulty" : "Hard", "question" : "6", "answer" : "6"},' +
-    '{"Difficulty" : "Hard", "question" : "7", "answer" : "7"},' +
-    '{"Difficulty" : "Hard", "question" : "8", "answer" : "8"}]';
+    '[{"Difficulty" : "Easy", "question" : "A tool for visualising of simple tabular data via charts and graphs", "answer" : "Spreadsheets"},' +
+    '{"Difficulty" : "Easy", "question" : "This tool is used to report, organize, alter and display data", "answer" : "Reporting Software"},' +
+    '{"Difficulty" : "Easy", "question" : "This stage of business intelligence is about the identification, evaluation of actions might be taken at different times", "answer" : "Risk Management"},' +
+    '{"Difficulty" : "Easy", "question" : "How many key stages are in business intelligence", "answer" : "Five"},' +
+    '{"Difficulty" : "Easy", "question" : "A process of protecting data from external attacks and data corruption", "answer" : "Data Security"},' +
+    '{"Difficulty" : "Easy", "question" : "In this process of BI, analysts explore data through visualization", "answer" : "Visual Analytics"},' +
+    '{"Difficulty" : "Easy", "question" : "A process of interpreting large data sets into graphs", "answer" : "Data Visualization"},' +
+    '{"Difficulty" : "Easy", "question" : "A management reporting tool to measure how well the organization company is performing", "answer" : "Dashboards" },' +
+    '{"Difficulty" : "Easy", "question" : "The discovery, interpretation and communication of meaningful patterns in data", "answer" : "Analytics"},' +
+    '{"Difficulty" : "Easy", "question" : "A set of data that gives information about some data", "answer" : "Metadata"},' +
+    '{"Difficulty" : "Normal", "question" : "A BI role who is responsible for designing the initial reporting templates and dashboards in the front-end applications", "answer" : "BI Application Designer"},' +
+    '{"Difficulty" : "Normal", "question" : "A system that enables users to create, read, update and delete data in a database", "answer" : "Database Management System"},' +
+    '{"Difficulty" : "Normal", "question" : "A process of defining, analysing and structuring data within data", "answer" : "Data Modelling"},' +
+    '{"Difficulty" : "Normal", "question" : "A data structure that stores values for a specific column in a table", "answer" : "Index"},' +
+    '{"Difficulty" : "Normal", "question" : "An arrangement of tables in a database such that the model resembles a snowflake shape", "answer" : "Snowflake Schema"},' +
+    '{"Difficulty" : "Normal", "question" : "This portion of a program usually interfaces directly with the end user", "answer" : "Front-end"},' +
+    '{"Difficulty" : "Normal", "question" : "A large store of data accumulated from a wide range of sources that can be processed within a company", "answer" : "Data Warehouse"},' +
+    '{"Difficulty" : "Normal", "question" : "A role that is responsible for directing a database and maintaining the data safe", "answer" : "Database Administrator"},' +
+    '{"Difficulty" : "Normal", "question" : "A style of data mart schema and most widely used approach to develop data warehouses", "answer" : "Star Schema"},' +
+    '{"Difficulty" : "Hard", "question" : "A process that organizes large business databases and supports complex analysis", "answer" : "Online Analytical processing"},' +
+    '{"Difficulty" : "Hard", "question" : "A way of storing data in a multidimensional form, generally for reporting purposes", "answer" : "OLAP Cube"},' +
+    '{"Difficulty" : "Hard", "question" : "A method of reading and updating all records in a data source during warehouse loading. It is one of the two techniques of loading data", "answer" : "Full Load"},' +
+    '{"Difficulty" : "Hard", "question" : "A graphical representation of the progress over time of users, toward some specified goal and highlighting them", "answer" : "Scoreboard"},' +
+    '{"Difficulty" : "Hard", "question" : "It is a term that refers data dimensions that can change slowly and unpredictably rather than on a static or fixed schedule", "answer" : "Slowly Changing Dimensions"},' +
+    '{"Difficulty" : "Hard", "question" : "It is a term that refers one or more fields in a database table that contain same value in a related table", "answer" : "Table Relations"}]';
